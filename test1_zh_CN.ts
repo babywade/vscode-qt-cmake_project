4d8fe317c10ad0277859ab4469ba77055c4ac94d
babywade <yixiaofei@stu.xjtu.edu.cn>
2021-12-19 23:22:17 +0800

    新建工程

---
+++ test1_zh_CN.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="test1_zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build/test1_autogen/include_Debug/ui_mainwindow.h" line="50"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
