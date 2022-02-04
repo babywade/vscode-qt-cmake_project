dced5d98243a6984252ad75c12948595a3bd9844
babywade <yixiaofei@stu.xjtu.edu.cn>
2022-02-05 00:25:47 +0800

    第十三章 对话框简介

--- main.cpp
+++ main.cpp
@@ -12,10 +12,10 @@
 
 int main(int argc, char *argv[])
 {
-    // QApplication a(argc, argv);
+    QApplication a(argc, argv);
     // mainwindow
-    // MainWindow w;
-    // w.show();
+    MainWindow w;
+    w.show();
 
     // label测试
     // QLabel label("Hello, world"); // 调用构造函数，java写法
@@ -38,23 +38,23 @@ int main(int argc, char *argv[])
     // return app.exec();
 
     // 布局管理器测试
-    QApplication app(argc, argv);
-    QWidget window;
-    window.setWindowTitle("Enter your age");
-    QSpinBox *spinBox = new QSpinBox(&window);
-    QSlider *slider = new QSlider(Qt::Horizontal, &window);
-    spinBox->setRange(0, 130);
-    slider->setRange(0, 130);
-    QObject::connect(slider, &QSlider::valueChanged, spinBox, &QSpinBox::setValue);
-    void (QSpinBox:: *spinBoxSignal)(int) = &QSpinBox::valueChanged;
-    QObject::connect(spinBox, spinBoxSignal, slider, &QSlider::setValue);
-    spinBox->setValue(35);
-    QHBoxLayout *layout = new QHBoxLayout; // 按照水平方向从左到右布局的布局管理器
-    layout->addWidget(spinBox);
-    layout->addWidget(slider);
-    window.setLayout(layout);
-    window.show();
-    return app.exec();
+    // QApplication app(argc, argv);
+    // QWidget window;
+    // window.setWindowTitle("Enter your age");
+    // QSpinBox *spinBox = new QSpinBox(&window);
+    // QSlider *slider = new QSlider(Qt::Horizontal, &window);
+    // spinBox->setRange(0, 130);
+    // slider->setRange(0, 130);
+    // QObject::connect(slider, &QSlider::valueChanged, spinBox, &QSpinBox::setValue);
+    // void (QSpinBox:: *spinBoxSignal)(int) = &QSpinBox::valueChanged;
+    // QObject::connect(spinBox, spinBoxSignal, slider, &QSlider::setValue);
+    // spinBox->setValue(35);
+    // QHBoxLayout *layout = new QHBoxLayout; // 按照水平方向从左到右布局的布局管理器
+    // layout->addWidget(spinBox);
+    // layout->addWidget(slider);
+    // window.setLayout(layout);
+    // window.show();
+    // return app.exec();
 
-    // return a.exec();
+    return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,20 +8,31 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    setWindowTitle(tr("Main Window"));
+    // setWindowTitle(tr("Main Window"));
 
+    // openAction = new QAction(QIcon("D:\\QtProjects\\vscode-qt-cmake_project\\images\\icon1.png"), tr("&Open..."), this);
+    // openAction->setShortcuts(QKeySequence::Open);
+    // openAction->setStatusTip(tr("Open an existing file"));
+    // connect(openAction, &QAction::triggered, this, &MainWindow::open);
+
+    // QMenu *file = menuBar()->addMenu(tr("&File"));
+    // file->addAction(openAction);
+
+    // QToolBar *toolBar = addToolBar(tr("&File"));
+    // toolBar->addAction(openAction);
+
+    // statusBar();
+
+    // 13 对话框
+    setWindowTitle(tr("Main Window"));
     openAction = new QAction(QIcon("D:\\QtProjects\\vscode-qt-cmake_project\\images\\icon1.png"), tr("&Open..."), this);
     openAction->setShortcuts(QKeySequence::Open);
     openAction->setStatusTip(tr("Open an existing file"));
     connect(openAction, &QAction::triggered, this, &MainWindow::open);
-
     QMenu *file = menuBar()->addMenu(tr("&File"));
     file->addAction(openAction);
-
     QToolBar *toolBar = addToolBar(tr("&File"));
     toolBar->addAction(openAction);
-
-    statusBar();
 }
 
 MainWindow::~MainWindow()
@@ -31,5 +42,15 @@ MainWindow::~MainWindow()
 
 void MainWindow::open()
 {
-    QMessageBox::information(this, tr("Information"), tr("Open"));
+    // QMessageBox::information(this, tr("Information"), tr("Open"));
+
+    // 无parent指针，任务栏显示
+    // QDialog dialog;
+    // dialog.setWindowTitle(tr("Hello, dialog!"));
+    // dialog.exec();
+
+    // 有parent指针，任务栏不显示
+    QDialog dialog(this);
+    dialog.setWindowTitle(tr("Hello, dialog!"));
+    dialog.exec();
 }
